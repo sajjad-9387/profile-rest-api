c5703f4b0ac3d3311be0c0cb1dd76ad3650a9e68
sajjad.ahmad <sajjad.ahmad@digital.homeoffice.gov.uk>
2024-11-27 12:59:18 +0000

    Added HelloAPI view

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test Api View"""
+
+
+    def get(self, request, format=None):
+        """Return a list of api views"""
+        an_apiview = [
+            'A',
+            'B',
+            'C',
+            'D',
+        ]
+
+        return Response({'message': 'HELLO', 'an_apiview': an_apiview})
